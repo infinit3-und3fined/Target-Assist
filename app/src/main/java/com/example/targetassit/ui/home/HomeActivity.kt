a5478f0015280c3a05be39b8976e8f2cbecbd993
infinit3-und3fined <infinit3undefined@gmail.com>
2025-05-27 20:51:52 +0500

    ......

--- app/src/main/java/com/example/targetassit/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/targetassit/ui/home/HomeActivity.kt
@@ -1,16 +1,26 @@
 package com.example.targetassit.ui.home
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.targetassit.R
+import com.example.targetassit.data.repository.DeviceInfoRepository
 
 class HomeActivity : AppCompatActivity() {
+    
+    private lateinit var viewModel: HomeViewModel
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         
-        // UI setup will be implemented later
+        // Initialize ViewModel
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        
+        // Initialize device info
+        val deviceInfoRepository = DeviceInfoRepository(this)
+        val deviceInfo = deviceInfoRepository.getDeviceInfo()
+        
+        // We'll implement UI updates later
     }
 } 
